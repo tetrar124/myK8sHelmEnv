968b69d4d9ea938adbc6fb2af542c06e03aa8481
tetrar124 <tetrar124@gmail.com>
2019-05-08 23:09:41 +0900

    5/8

--- 1_ubuntuMysql.bat
+++ 1_ubuntuMysql.bat
@@ -1,6 +1,8 @@
 kubectl apply -f localstrage.yaml
 kubectl apply -f persistentVol.yaml
 kubectl apply -f pvc.yaml
+kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=tetrar124 --docker-password=liwata2--3 --docker-email=tetrar124@gmail.com
 kubectl apply -f ubuntu.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.8.3/src/deploy/recommended/kubernetes-dashboard.yaml
 helm init
 helm install --name mysql stable/mysql -f mysqlConfig.yaml
--- 2_rollback.bat
+++ 2_rollback.bat
@@ -1,4 +1,5 @@
 helm del mysql --purge
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.8.3/src/deploy/recommended/kubernetes-dashboard.yaml
 kubectl delete -f ubuntu.yaml
 kubectl delete -f pvc.yaml
 kubectl delete -f persistentVol.yaml
