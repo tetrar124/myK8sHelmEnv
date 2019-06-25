6aa745c9aea575f3cfd7bd65a0aaf63d54508a61
tetrar124 <36783730+tetrar124@users.noreply.github.com>
2019-06-25 20:17:24 +0900

    6/25

--- windows/startwindows.bat
+++ windows/startwindows.bat
@@ -1,7 +1,7 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install -y vivaldi
 choco install -y anaconda3
 choco install -y docker-for-windows
-choco install -y vivaldi
 choco install -y git.install
 choco install -y mendeley
 choco install -y pycharm-community
