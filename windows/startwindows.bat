97b5e695039d8021011a4fb224a659e91a9374f1
tetra <tetrar124@gmail.com>
2019-07-27 15:52:31 +0900

    7/27

--- windows/startwindows.bat
+++ windows/startwindows.bat
@@ -1,8 +1,12 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install -y vivaldi
 choco install -y anaconda3
+#add PAtH
+setx PATH "%PATH%;C:\tools\Anaconda3\Scripts"
+
+choco install -y libreoffice-fresh
 choco install -y docker-for-windows
-choco install -y git.install
+choco install -y git.install --params "/GitAndUnixToolsOnPath"
 choco install -y mendeley
 choco install -y pycharm-community
 choco install -y github-desktop
@@ -13,3 +17,14 @@ choco install -y notepadplusplus
 choco install -y slack
 choco install -y mousewithoutborders
 choco install -y gimp
+choco install -y wsl-ubuntu-1804
+choco install -y steam
+pip install lightgbm
+pip install -U rgf_python
+pip install -U xgboost
+pip install -U bayesian-optimization
+pip install -U mlxtend
+pip install tensorflow-gpu==2.0.0-alpha0
+pip install python-louvain
+conda install -y cudnn cudatoolkit numba
+conda install -c rdkit rdkit
